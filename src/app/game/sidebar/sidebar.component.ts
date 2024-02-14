d051ca4cab8b1da4d6b47e77de92c3d86a3b1dd9
Andy Attwell <andyattwell@gmail.com>
2024-02-13 22:06:33 -0300

    Global rotate and orbit settings

--- src/app/game/sidebar/sidebar.component.ts
+++ src/app/game/sidebar/sidebar.component.ts
@@ -2,11 +2,11 @@ import { Component, Input, Output, AfterViewInit, EventEmitter, Inject } from '@
 import { Planet } from "../Planet";
 import { MatListModule } from '@angular/material/list';
 import { MatButtonModule } from '@angular/material/button';
-
+import { MatCheckboxModule } from '@angular/material/checkbox';
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [MatListModule, MatButtonModule],
+  imports: [MatListModule, MatButtonModule, MatCheckboxModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
@@ -16,10 +16,26 @@ export class SidebarComponent {
   @Output() selectEvent = new EventEmitter<any>();
 
   selectedOpt = '';
+  orbitEnabled = false;
+  rotationEnabled = true;
 
   selectPlanet(event:MouseEvent, planet: Planet) {
     event.preventDefault();
     this.selectEvent.emit(planet);
     this.selectedOpt = planet.name
+  }
+
+  toggleOrbit(enabled: boolean) {
+    this.orbitEnabled = enabled
+    this.planets.forEach(p => {
+      p.followOrbit = enabled
+    })
+  }
+
+  toggleRotation(enabled: boolean) {
+    this.rotationEnabled = enabled
+    this.planets.forEach(p => {
+      p.rotate = enabled
+    })
   }
 }
